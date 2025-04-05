ec4bd5d87c57b5fd43cce998182db2c0478a2bd4
Haymi-mes <hewitderessa@gmail.com>
2025-04-05 13:56:34 -0700

    db

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 const db = mysql.createPool({
-  host: "82.197.82.42",
-  user: "u677091426_himeynathan",
-  password: "/ANglb8qYt1",
-  database: "u677091426_himeynathan",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 
